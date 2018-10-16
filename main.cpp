30337245d96f243c96e3ff56d0869d80409584e7
Stjepan Mocilac <mocilac.stjepan@gmail.com>
2018-10-16 13:21:37 +0200

    some update

--- main.cpp
+++ main.cpp
@@ -6,40 +6,95 @@
 
 using namespace std;
 
+/* testa funkcija, demonstrira samo prvi zadatak iz vjezbe */
+void zadatak1();
+
+/* testna funkcija, usporeduje LU i LUP algoritam */
+void rijesi_sustav(std::string file_A, std::string file_b);
+
+/* teste funckije za LU i LUP */
+void test_using_LU(Matrica<> A, Matrica<> b);
+void test_using_LUP(Matrica<> A, Matrica<> b);
+
+
 int main()
 {
+    zadatak1();
 
-    Matrica<> mat1("mat1.txt");
-    Matrica<> b("b.txt");
-    mat1.LU_decompose();
-    cout << Matrica<double>::backward_substitution(mat1, Matrica<>::forward_substitution(mat1, b));
-    //cout << mat1;
-
-//    Matrica<> m(4, 4);
-//    Matrica<> m2(4, 4);
-
-
-//    m2 += 3;
-//    m += 2;
-//    m2 *= 3;
-
-//    cout << m2 ;
-
-//    ifstream myfile;
-//    myfile.open ("example.txt");
-//    myfile >> m;
-
-//    Matrica<> m5("example.txt");
-//    cout << m5;
-
-//    m3 = ~m5;
-//    std::cout << endl;
-
-//    cout << m3;
-
-//    ofstream myOutFile;
-//    myOutFile.open("stored_matrix.txt");
-//    myOutFile << m5;
+    //rijesi_sustav("mat1_zad2.txt", "mat2_zad2.txt");
+    //rijesi_sustav("mat1_zad3.txt", "mat2_zad3.txt");
+    //rijesi_sustav("mat1_zad4.txt", "mat2_zad4.txt"); // TODO: LUP daje tocne vrijednost bez decimala.Zasto?
+    //rijesi_sustav("mat1_zad5.txt", "mat2_zad5.txt"); // TODO: warning da su jako male vrijednost
+    //rijesi_sustav("mat1_zad6.txt", "mat2_zad6.txt"); // TODO: oboje radi, moze imati problema. Zasto?
 
     return 0;
 }
+
+
+void zadatak1(){
+    Matrica<double> mat1("mat_zad1.txt");
+
+    Matrica<double> mat = mat1 * 3;
+    mat /= 3;
+
+    std::cout << mat << mat1;
+    std::cout << (mat == mat1) << std::endl;
+}
+
+
+void rijesi_sustav(std::string file_A, std::string file_b){
+    Matrica<double> A(file_A);
+    Matrica<double> b(file_b);
+
+    cout << "--------------------LU-----------------------" << endl;
+    test_using_LU(A, b);
+    cout << endl <<  "--------------------LUP-----------------------" << endl;
+    test_using_LUP(A, b);
+}
+
+
+void test_using_LU(Matrica<> A, Matrica<> b){
+    cout << "A = " << std::endl << A;
+    cout << endl << "b = " << std::endl << b;
+
+    if (A.LU_decompose()){
+        std::cerr << "WARNING: could not decompose using LU algorithm, solution may not be valid." << std::endl;
+    }
+
+    cout << endl << "After LU: " << std::endl << A;
+
+    Matrica<> y = Matrica<>::forward_substitution(A, b);
+    cout << endl << "Forward substitution vector: " << std::endl << y;
+
+    cout << endl << "Backward substitution vector: " << std::endl;
+
+    Matrica<> x = Matrica<double>::backward_substitution(A, y);
+    if (x.array().size() == 0)
+        std::cerr << "WARNING: Backward supstitution :: divide by zero !" << std::endl;
+    else
+        std::cout << endl << Matrica<double>::backward_substitution(A, y);
+}
+
+
+void test_using_LUP(Matrica<> A, Matrica<> b){
+    cout << "A = " << std::endl << A;
+    cout << endl << "b = " << std::endl << b;
+
+    if (A.LUP_decompose()){
+        std::cerr << "WARNING: could not decompose using LUP algorithm, solution may not be valid." << std::endl;
+    }
+
+    cout << endl << "After LUP: " << std::endl << A;
+    cout << endl << "Permutation matrix: " << std::endl << Matrica<>::get_permutations(A);
+    b = Matrica<>::get_permutations(A) * b;
+    Matrica<> y = Matrica<>::forward_substitution(A, b);
+    cout << endl << "Forward substitution vector: " << std::endl << y;
+
+    Matrica<> x = Matrica<double>::backward_substitution(A, y);
+
+    if (x.array().size() == 0)
+        std::cerr << "WARNING: Backward supstitution :: divide by zero !" << std::endl;
+    else
+        cout << endl << "Backward substitution vector: " << std::endl << x;
+}
+
--- matrica.hpp
+++ matrica.hpp
@@ -16,6 +16,7 @@ template <class T = double>
 class Matrica
 {
 public:
+    Matrica(const std::valarray<T> &va, size_t x, size_t y) ;
     Matrica(const std::pair<size_t, size_t>&);
     Matrica(size_t x, size_t y);
     Matrica(const Matrica&);
@@ -23,7 +24,13 @@ public:
 
     ~Matrica();
 
+    void init_permutations();
+    void replace_rows(size_t, size_t);
+    void replace_columns(size_t, size_t);
+
     int LU_decompose();
+    int LUP_decompose();
+    size_t find_max_below_elem(size_t, size_t);
     static Matrica forward_substitution(const Matrica<T> &A, const Matrica<T> &b);
     static Matrica backward_substitution(const Matrica<T> &A, const Matrica<T> &b);
 
@@ -42,6 +49,9 @@ public:
     Matrica& operator-=(const Matrica&);
     Matrica operator-(const Matrica&);
 
+
+    Matrica& operator/=(T);
+    Matrica operator/(T);
     Matrica& operator*=(T);
     Matrica operator*(T);
     Matrica operator*(const Matrica&);
@@ -60,43 +70,93 @@ public:
     std::valarray<T> &array() {return values;}
     std::valarray<T> array() const {return values;}
 
+    static Matrica<T> get_permutations(const Matrica<T> &matrica);
+
     std::pair<size_t, size_t> get_dimensions() const{return std::pair<size_t, size_t>(m_x, m_y);}
 
     size_t row_size() const { return m_y;}
     size_t col_size() const { return m_x;}
 
-
+    static const T epsilon;
 
 private:
 
     std::valarray<T> values;
+    std::valarray<T> permutations;
 
     size_t m_x;
     size_t m_y;
 };
 
 
+/**
+ * Proizvoljno veliki epsilon. Defaultna vrijednost 1e-10
+ */
 template<class T>
-Matrica<T>::Matrica(const std::pair<size_t, size_t> &dimension) : m_x(dimension.first), m_y(dimension.second), values(std::valarray<T>(dimension.first * dimension.second))
-{
+const T Matrica<T>::epsilon = 0.0000000001;
 
+
+/**
+ *  Funkcija dohvaca permutacijsku matricu.
+ */
+template<class T>
+Matrica<T> Matrica<T>::get_permutations(const Matrica<T> &matrica){
+    return Matrica<T>(matrica.permutations, matrica.m_x, matrica.m_y);
 }
 
 
+/**
+ * Konstruktor, prima inicijalno polje te x i y dimenziju matrice.
+ * Korisnik se mora osigurati da je ulazno polje (barem) velicine x * y
+ */
 template<class T>
-Matrica<T>::Matrica(size_t x, size_t y) : m_x(x), m_y(y), values(std::valarray<T>(x * y))
+Matrica<T>::Matrica(const std::valarray<T> &va, size_t x, size_t y) : m_x(x), m_y(y), values(va), permutations(x*y)
 {
-
+    init_permutations();
 }
 
 
+/**
+ * Konstruktor, prima x i y dimenziju matrice.
+ * Matrica se inicijalizira nulama.
+ */
 template<class T>
-Matrica<T>::Matrica(const Matrica<T> &mat) : m_x(mat.col_size()), m_y(mat.row_size()), values(std::valarray<T>(mat.array()))
+Matrica<T>::Matrica(const std::pair<size_t, size_t> &dimension) : permutations(std::valarray<T>(dimension.first * dimension.second)), m_x(dimension.first), m_y(dimension.second), values(std::valarray<T>(dimension.first * dimension.second))
 {
-
+    init_permutations();
 }
 
 
+/**
+ * Konstruktor, prima x i y dimenziju matrice.
+ * Matrica se inicijalizira nulama.
+ */
+template<class T>
+Matrica<T>::Matrica(size_t x, size_t y) : m_x(x), m_y(y), values(std::valarray<T>(x * y)),permutations(std::valarray<T>(x * y))
+{
+    init_permutations();
+}
+
+
+/**
+ * Copy constructor, kao argument prima matricu cije vrijednosti i permutacijsku tablicu kopira.
+ * Napomena: kopiraju se samo vrijednosti, ne i permutacijska matrica!
+ */
+template<class T>
+Matrica<T>::Matrica(const Matrica<T> &mat) : m_x(mat.col_size()), m_y(mat.row_size()), values(std::valarray<T>(mat.array())), permutations(std::valarray<T>(mat.array().size()))
+{
+    init_permutations();
+}
+
+
+/**
+ * Konstruktor, argument je path do filea koji treba otvoriti.
+ * Vrijednosti unutar filea moraju biti formata:
+ * x11 x12 ... x1n \n
+ *       ....
+ * xm1 xm2 ... xmn \n
+ *
+ */
 template<class T>
 Matrica<T>::Matrica(const std::string &file_name)
 {
@@ -118,9 +178,11 @@ Matrica<T>::Matrica(const std::string &file_name)
     }
 
     values = std::valarray<T>(vals.data(), vals.size());
-
+    permutations = std::valarray<T>(vals.size());
     m_x = row;
     m_y = values.size() / row;
+
+    init_permutations();
 }
 
 
@@ -131,18 +193,137 @@ Matrica<T>::~Matrica()
 }
 
 
+/**
+ * Funkcija koja inicijalizira permutacijsku matricu, odnosno na dijagonalama postavlja jedinice.
+ * Sve vrijednost van glavne dijagonale su nula.
+ */
+template<class T>
+void Matrica<T>::init_permutations(){
+    for(size_t i = 0, j = 0; i < m_x && j < m_y; i++, j++)
+        permutations[j*m_x + i] = 1;
+}
+
+
+/**
+ * Zamjenjuje redak r1 sa redkom r2 u matrici. Azurira permutacijsku matricu sukladno tome.
+ */
+template<class T>
+void Matrica<T>::replace_rows(size_t r1, size_t r2){
+    if (r1 >= m_y || r2 >= m_y || r1 == r2)
+        return;
+
+    for (size_t i = 0; i < m_x; i++){
+        // replace permutation matrix rows
+        T tmp = permutations[r1*m_x + i];
+        permutations[r1*m_x + i] = permutations[r2*m_x + i];
+        permutations[r2*m_x + i] = tmp;
+        // replace values matrix rows
+        tmp = values[r1*m_x + i];
+        values[r1*m_x + i] = values[r2*m_x + i];
+        values[r2*m_x + i] = tmp;
+    }
+}
+
+
+/**
+ * Zamjenjuje stupac c1 sa stupcom c2 u matrici. Azurira permutacijsku matricu sukladno tome.
+ */
+template<class T>
+void Matrica<T>::replace_columns(size_t c1, size_t c2){
+    if (c1 >= m_x || c2 >= m_x)
+        return;
+
+    for (size_t i = 0; i < m_x; i++){
+        // replace permutation matrix rows
+        T tmp = permutations[i*m_x + c1];
+        permutations[i*m_x + c1] = permutations[i*m_x + c2];
+        permutations[i*m_x + c2] = tmp;
+        // replace values matrix rows
+        tmp = values[i*m_x + c1];
+        values[i*m_x + c1] = values[i*m_x + c2];
+        values[i*m_x + c2] = tmp;
+    }
+}
+
+
+/**
+ * Funkcija radi LU dekompoziciju matrice.
+ */
 template <class T>
 int Matrica<T>::LU_decompose(){
+    int is_error = 0;
+
     for (size_t i = 0; i < m_y - 1; i++){
         for (size_t j = i + 1; j < m_y; j++){
+            // divide with zero flag
+            if (std::abs((*this)(i, i)) <= Matrica<T>::epsilon)
+                is_error = 1;
+
             (*this)(i, j) /= (*this)(i, i);
             for (size_t k = i + 1; k < m_x; k++){
                 (*this)(k, j) -= (*this)(i,j) * (*this)(k,i);
             }
         }
     }
+
+    return is_error;
 }
 
+
+/**
+ * Funkcija radi LUP dekompoziciju matrice.
+ */
+template <class T>
+int Matrica<T>::LUP_decompose(){
+    int is_error = 0;
+
+    for (size_t i = 0; i < m_y - 1; i++){
+
+        size_t max_row = find_max_below_elem(i, i);
+        replace_rows(i, max_row);
+
+        for (size_t j = i + 1; j < m_y; j++){
+
+            // divide with zero flag
+            if (std::abs((*this)(i, i)) <= Matrica<T>::epsilon)
+                is_error = 1;
+
+            (*this)(i, j) /= (*this)(i, i);
+            for (size_t k = i + 1; k < m_x; k++){
+                (*this)(k, j) -= (*this)(i,j) * (*this)(k,i);
+            }
+        }
+    }
+
+    return is_error;
+}
+
+
+/**
+ * Funkcija pronalazi najveci element u stupcu x ispod redka y.
+ */
+template <class T>
+size_t Matrica<T>::find_max_below_elem(size_t x, size_t y){
+
+    if (x >= m_x || y >= m_y)
+        return m_y;
+    size_t row_index = y;
+
+    T _max = std::abs((*this)(x, y));
+
+    for (size_t i = x, j = y + 1; j < m_y & i < m_x; j++){
+        if (std::abs((*this)(x, j)) > _max){
+            _max = std::abs((*this)(x, j));
+            row_index = j;
+        }
+    }
+    return row_index;
+}
+
+
+/**
+ * Unaprijedna supstitucija (koristi se dio matrice A ispod glavne dijagonale.)
+ */
 template <class T>
 Matrica<T> Matrica<T>::forward_substitution(const Matrica<T> &A, const Matrica<T> &b){
     Matrica<T> y(b);
@@ -156,6 +337,9 @@ Matrica<T> Matrica<T>::forward_substitution(const Matrica<T> &A, const Matrica<T
 }
 
 
+/**
+ * Supstitucija unatrag (koristi se dio matrice A iznad glavne dijagonale i glavna dijagonala)
+ */
 template <class T>
 Matrica<T> Matrica<T>::backward_substitution(const Matrica<T> &A, const Matrica<T> &y){
     Matrica<T> x(y);
@@ -165,6 +349,10 @@ Matrica<T> Matrica<T>::backward_substitution(const Matrica<T> &A, const Matrica<
         for (int j = i + 1; j < x.row_size(); j++){
             x(0, i) -= A(j, i) * x(0, j);
         }
+
+        if (std::abs(A(i, i)) <= Matrica<T>::epsilon)
+            return Matrica<>(0, 0);
+
         x(0, i) /= A(i, i);
 
     }
@@ -181,6 +369,9 @@ Matrica<T> &Matrica<T>::operator=(const Matrica<T> &mat)
 }
 
 
+/**
+ * indeksira elemente matrice u 2d prostoru.
+ */
 template<class T>
 T &Matrica<T>::operator()(size_t x, size_t y)
 {
@@ -188,6 +379,9 @@ T &Matrica<T>::operator()(size_t x, size_t y)
 }
 
 
+/**
+ * indeksira elemente matrice u 2d prostoru (const)
+ */
 template<class T>
 T Matrica<T>::operator()(size_t x, size_t y) const
 {
@@ -273,6 +467,28 @@ Matrica<T> Matrica<T>::operator-(const Matrica<T> &mat)
 }
 
 
+
+template<class T>
+Matrica<T> &Matrica<T>::operator/=(T t)
+{
+    values /= t;
+
+    return *this;
+}
+
+
+template<class T>
+Matrica<T> Matrica<T>::operator/(T t)
+{
+    Matrica<T> mat(*this);
+
+    mat.array() /= t;
+
+    return mat;
+}
+
+
+
 template<class T>
 Matrica<T> &Matrica<T>::operator*=(T t)
 {
@@ -293,10 +509,13 @@ Matrica<T> Matrica<T>::operator*(T t)
 }
 
 
-
-//    a b c      q w       (a*q+b*z+c*v)  (a*w+b*x+c*b)
-//    d e f      z x   =   (d*q+e*z+f*v)  (d*w+e*x+f*b)
-//               v b
+/**
+ * Mnozenje matrica (kartezijev produkt)
+ *
+ *    a b c      q w       (a*q+b*z+c*v)  (a*w+b*x+c*b)
+ *    d e f      z x   =   (d*q+e*z+f*v)  (d*w+e*x+f*b)
+ *               v b
+ */
 template<class T>
 Matrica<T> Matrica<T>::operator*(const Matrica<T> &mat)
 {
@@ -315,6 +534,7 @@ Matrica<T> Matrica<T>::operator*(const Matrica<T> &mat)
     return mat_out;
 }
 
+
 template <class T>
 Matrica<T> Matrica<T>::operator~(){
     Matrica<T> mat(m_y, m_x);
@@ -331,7 +551,20 @@ Matrica<T> Matrica<T>::operator~(){
 template<class T>
 bool Matrica<T>::operator==(const Matrica<T> &mat)
 {
-    return values == mat.array();
+    // valarray ima interno definiran komparator nad svim elementima.
+    return (values == mat.array()).min();
+
+    // Ako bude potrebe moze se usporedba svesti na ovisnost o nekom nasem epsilonu
+    /*auto mat_array_ptr = mat.array();
+
+    if (mat_array_ptr.size() != values.size())
+        return false;
+
+    for ( int i = 0 ; i < values.size(); i++){
+        if (std::abs(values[i] - mat_array_ptr) > Matrica<T>::epsilon)
+            return false;
+    }
+    return true;*/
 }
 
 
